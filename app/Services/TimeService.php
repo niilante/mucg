d5d248ebf0eae922d7a2bda6b2d352b01809c7d7
niilante <niilante@edenteq.com>
2020-02-19 22:57:46 -0500

    wip: code sniffer initialized for app\services

--- app/Services/TimeService.php
+++ app/Services/TimeService.php
@@ -11,12 +11,14 @@ class TimeService
         $time = Carbon::parse($from);
         $timeRange = [];
 
-        do 
-        {
-            array_push($timeRange, [
+        do {
+            array_push(
+                $timeRange,
+                [
                 'start' => $time->format("H:i"),
                 'end' => $time->addMinutes(30)->format("H:i")
-            ]);    
+                ]
+            );
         } while ($time->format("H:i") !== $to);
 
         return $timeRange;
