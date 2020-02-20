756747a1c4ffa245f7ef58c68bc7226af1f10ed6
niilante <niilante@edenteq.com>
2020-02-19 22:54:55 -0500

    wip: code sniffer initialized for routes

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,21 @@
 <?php
 
-Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']], function () {
-    // Permissions
-    Route::apiResource('permissions', 'PermissionsApiController');
+Route::group(
+    ['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', 'middleware' => ['auth:api']],
+    function () {
+        // Permissions
+        Route::apiResource('permissions', 'PermissionsApiController');
 
-    // Roles
-    Route::apiResource('roles', 'RolesApiController');
+        // Roles
+        Route::apiResource('roles', 'RolesApiController');
 
-    // Users
-    Route::apiResource('users', 'UsersApiController');
+        // Users
+        Route::apiResource('users', 'UsersApiController');
 
-    // Lessons
-    Route::apiResource('lessons', 'LessonsApiController');
+        // Lessons
+        Route::apiResource('lessons', 'LessonsApiController');
 
-    // School Classes
-    Route::apiResource('school-classes', 'SchoolClassesApiController');
-});
+        // School Classes
+        Route::apiResource('school-classes', 'SchoolClassesApiController');
+    }
+);
