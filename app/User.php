211975b32565659d4a2a35711466ba0bd2b4bc98
niilante <richardlawson97@gmail.com>
2020-08-26 03:33:17 +0000

    wip: ui enhancement for views implemented

--- app/User.php
+++ app/User.php
@@ -92,4 +92,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(SchoolClass::class, 'class_id');
     }
+
+    // function classes()
+    // {
+    //     return $this->belongsToMany(SchoolClass::class);
+    // }
 }
