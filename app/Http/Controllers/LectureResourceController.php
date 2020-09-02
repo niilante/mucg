ba0fe2507877a9664c2d1f037e94d24cbac60b99
niilante <richardlawson97@gmail.com>
2020-09-02 13:11:52 +0000

    wip: this department model -crmf was added to fix #18

---
+++ app/Http/Controllers/LectureResourceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\LectureResource;
+use Illuminate\Http\Request;
+
+class LectureResourceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\LectureResource  $lectureResource
+     * @return \Illuminate\Http\Response
+     */
+    public function show(LectureResource $lectureResource)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\LectureResource  $lectureResource
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(LectureResource $lectureResource)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\LectureResource  $lectureResource
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, LectureResource $lectureResource)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\LectureResource  $lectureResource
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(LectureResource $lectureResource)
+    {
+        //
+    }
+}
