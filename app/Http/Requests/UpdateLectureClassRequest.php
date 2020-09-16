c224d32005515ca2393edea46528b4be2b896e7d
niilante <richardlawson97@gmail.com>
2020-09-16 07:13:24 +0000

    wip: requests enhanced fixes #27

--- app/Http/Requests/UpdateLectureClassRequest.php
+++ app/Http/Requests/UpdateLectureClassRequest.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Requests;
 
-use App\SchoolClass;
+use App\LectureClass;
 use Gate;
 use Illuminate\Foundation\Http\FormRequest;
 use Symfony\Component\HttpFoundation\Response;
 
-class UpdateSchoolClassRequest extends FormRequest
+class UpdateLectureClassRequest extends FormRequest
 {
     public function authorize()
     {
-        abort_if(Gate::denies('school_class_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+        abort_if(Gate::denies('lecture_class_edit'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return true;
     }
