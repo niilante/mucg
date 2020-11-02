7afe4d64ff151c5e49b2d022c1a3fbe7db934a69
niilante <richardlawson97@gmail.com>
2020-11-02 00:50:25 +0000

    wip: department controller modified

--- app/Http/Controllers/Admin/DepartmentController.php
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Department;
 use Illuminate\Http\Request;
+use App\Http\Requests\MassDestroyDepartmentRequest;
+use App\Http\Requests\StoreDepartmentRequest;
+use App\Http\Requests\UpdateDepartmentRequest;
 use Gate;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Controllers\Controller;
@@ -21,12 +24,20 @@ class DepartmentController extends Controller
 
     public function create()
     {
-        //
+        abort_if(Gate::denies('department_access'), Response::HTTP_FORBIDDEN, '403 Forbidden');
+
+        // $data[''] = Department::orderBy('updated_at', 'DESC')->get();
+
+        return view('admin.departments.create');
     }
 
-    public function store(Request $request)
+    public function store(StoreDepartmentRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        Department::create($data);
+
+        return redirect()->route('admin.departments.index');
     }
 
     public function show(Department $department)
@@ -45,13 +56,24 @@ class DepartmentController extends Controller
         return view('admin.departments.edit', $data);
     }
 
-    public function update(Request $request, Department $department)
+    public function update(UpdateDepartmentRequest $request, Department $department)
     {
-        //
+        $data = $request->validated();
+
+        $data['department'] = $department->update($data);
+
+        return redirect()->route('admin.departments.index');
     }
 
     public function destroy(Department $department)
     {
         //
+    }
+
+    public function massDestroy(MassDestroyDepartmentRequest $request)
+    {
+        Department::whereIn('id', request('ids'))->delete();
+
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
