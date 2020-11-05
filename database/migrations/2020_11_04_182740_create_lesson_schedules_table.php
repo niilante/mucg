32b9c1df96778869a5fc90272d61e9f6c9964fdc
niilante <richardlawson97@gmail.com>
2020-11-05 08:18:50 +0000

    wip:  migration fixes with @eliks

--- database/migrations/2020_11_04_182740_create_lesson_schedules_table.php
+++ database/migrations/2020_11_04_182740_create_lesson_schedules_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLectureTypesTable extends Migration
+class CreateLessonSchedulesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateLectureTypesTable extends Migration
      */
     public function up()
     {
-        Schema::create('lecture_types', function (Blueprint $table) {
+        Schema::create('lesson_schedules', function (Blueprint $table) {
             $table->bigIncrements('id');
-
-            $table->string('name');
-            $table->string('code', 10);
-            $table->text('description');
-            $table->integer('added_by_id')->unsigned()->index()->nullable();
             
+            $table->integer('lecture_hall_id')->unsigned()->index();
+            $table->integer('lesson_id')->unsigned()->index();
+            $table->time('start_time');
+            $table->time('end_time');
+
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ class CreateLectureTypesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('lecture_types');
+        Schema::dropIfExists('lesson_schedules');
     }
 }
