33a2e2ef06afe550ed15790b9bd31ef9b241c26f
niilante <richardlawson97@gmail.com>
2020-11-05 08:18:40 +0000

    wip:  factory fixes with @eliks

---
+++ database/factories/LessonScheduleFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\LessonSchedule;
+use Faker\Generator as Faker;
+
+$factory->define(LessonSchedule::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
