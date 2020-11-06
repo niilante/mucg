f29a47a204861ce89fbc08e7fe0c300c030fd85a
niilante <richardlawson97@gmail.com>
2020-11-06 23:33:11 +0000

    wip: controller modified

---
+++ app/Http/Controllers/SessionWeekDayController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\SessionWeekDay;
+use Illuminate\Http\Request;
+
+class SessionWeekDayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\SessionWeekDay  $sessionWeekDay
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SessionWeekDay $sessionWeekDay)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\SessionWeekDay  $sessionWeekDay
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SessionWeekDay $sessionWeekDay)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\SessionWeekDay  $sessionWeekDay
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SessionWeekDay $sessionWeekDay)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\SessionWeekDay  $sessionWeekDay
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SessionWeekDay $sessionWeekDay)
+    {
+        //
+    }
+}
